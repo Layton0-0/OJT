dca670d7a240cc345a6764c7827c41d49d285089
Yunjeong Choi / 최윤정 <92416563+Layton0-0@users.noreply.github.com>
2022-06-08 13:05:50 +0900

    파일 업로드 과제 완료(수정) 한얼님 피드백 적용

--- src/main/java/com/yoon/practice/board/file/FileInfoController.java
+++ src/main/java/com/yoon/practice/board/file/FileInfoController.java
@@ -13,9 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/file")
@@ -33,7 +39,7 @@ public class FileInfoController {
     private String imagePath = "\\image\\";
 
     // 파일 정보 읽어오기
-    @GetMapping("/{type}/{file_name}")
+//    @GetMapping("/{type}/{file_name}")
     public ResponseEntity<DataResponse> getFileData(@PathVariable("type") String type, @PathVariable("file_name") String fileName){
         String filePath = "\\" + type + "\\" + fileName;
         FileInfo fileInfo = fileInfoService.findByFilePath(filePath);
@@ -49,6 +55,35 @@ public class FileInfoController {
         return new ResponseEntity<>(dataResponse, httpHeaders, HttpStatus.OK);
     }
 
+    // 한얼님 방식 적용(파일 바로 띄우기)
+    @GetMapping("/{type}/{file_name}")
+    public void getFileItself(@PathVariable("type") String type, @PathVariable("file_name") String fileName, HttpServletResponse response) throws IOException {
+        String filePath = "\\" + type + "\\" + fileName;
+        FileInfo fileInfo = fileInfoService.findByFilePath(filePath);
+        filePath = baseDir + filePath;
+
+        File file = new File(filePath);
+
+        long fileSize = file.length();
+        byte[] buffer = new byte[(int) fileSize];
+
+        // 이미지로 콘텐츠 타입 설정 -> 브라우저에 나옴
+        response.setContentType(fileInfo.getFileExtention());
+//        response.setContentLength((int) fileSize);
+
+        FileInputStream inputStream = new FileInputStream(file);
+        ServletOutputStream outputStream = response.getOutputStream();
+
+        inputStream.read(buffer);
+        outputStream.write(buffer);
+
+//        outputStream.flush(); -> 사이즈에 맞게 read해서 필요없음
+
+        outputStream.close();
+        inputStream.close();
+
+    }
+
     // MultipartFile 파라미터 이름은 request의 body의 key이름과 같아야 함.
     @PostMapping(value = "/upload")
     public ResponseEntity<DataResponse> uploadFile(MultipartFile file, String boardCode) throws IOException {
@@ -60,10 +95,16 @@ public class FileInfoController {
         dataResponse.setMessage(StatusEnum.OK.code);
 
         FileInfo newFile = fileInfoService.findByFilePath(imagePath + file.getOriginalFilename());
+        File path = new File(baseDir + imagePath +file.getOriginalFilename());
+
+        // 이미 있는 파일일 경우 파일 업데이트
         if(newFile != null) {
+            file.transferTo(path);
+
             dataResponse.setData(newFile);
             return new ResponseEntity<>(dataResponse, httpHeaders, HttpStatus.OK);
         }
+
         newFile = new FileInfo();
 
         if(!file.isEmpty()){
@@ -74,7 +115,6 @@ public class FileInfoController {
             Board board = boardService.getReferenceById(boardCode);
             newFile.setBoard(board);
 
-            File path = new File(baseDir + imagePath +file.getOriginalFilename());
             // 경로가 없을 경우 만들기
             if(!path.exists()){
                 path.mkdirs();
@@ -84,7 +124,6 @@ public class FileInfoController {
             newFile = fileInfoService.save(newFile);
 
         }
-
 
         dataResponse.setData(newFile);
 
